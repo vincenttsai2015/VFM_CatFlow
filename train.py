441c70d56310ee46ff994316ef3d4e43d03fb01a
vincenttsai2015 <vincentthunder2011@gmail.com>
2026-02-28 22:41:49 +0800

    Fix training_graph_epoch

--- train.py
+++ train.py
@@ -91,80 +91,15 @@ def train_graph_epoch(train, model, loader, optimizer, lr_scheduler, warmup_sche
         tau_t = get_tau_sched(tau_sched)
         x_1, e_1 = graph.X.float(), graph.E.float()
 
-        y_t = torch.squeeze(t).unsqueeze(-1)
-        pred = model(x_t.float(), e_t.float(), y_t.float(), mask)
-
-        # for k in range(10):
-        #
-        #     x_1e = x_1[k]
-        #     e_1e = e_1[k]
-        #     x_0e = torch.randn_like(x_1e)
-        #     e_0e = torch.randn_like(e_1e)
-        #     e_0e = 0.5 * (e_0e + e_0e.transpose(0, 1))
-        #     maske = mask[k]
-        #     mols = []
-        #     for t in torch.linspace(0, 1, 5):
-        #         x_te = t * x_1e + (1 - t) * x_0e
-        #         e_te = t * e_1e + (1 - t) * e_0e
-        #
-        #         mol = make_molecule(x_te, e_te, sum(maske), True)
-        #         mols.append(mol)
-        #         # visualize RDKit grid
-        #     grid = Chem.Draw.MolsToGridImage(mols, molsPerRow=5, subImgSize=(1000, 1000))
-        #     grid.show()
-        # quit()
-        #
-        #
-
-
-        # x_1 = torch.einsum('bni,ij->bnj', x_1.float(), node_simplex)
-        # e_1 = torch.einsum('bni,ij->bnj', e_1.float(), edge_simplex)
-
-
-        # gumbel_x = -torch.log(-torch.log(torch.rand_like(x_1.float()) + 1e-10) + 1e-10)
-        # gumbel_e = -torch.log(-torch.log(torch.rand_like(e_1.float()) + 1e-10) + 1e-10)
-        # gumbel_e = 0.5 * (gumbel_e + gumbel_e.transpose(1, 2))
-        # x_0, e_0 = torch.ones_like(x_1) / 4, torch.ones_like(e_1) / 4
-        #
-        # all_mols = []
-        # for t in torch.linspace(0, 1, 10):
-        #     x_t = torch.softmax((torch.log(t * x_1 + (1 - t) * x_0) + gumbel_x) / tau_t(t), dim=-1)
-        #     e_t = torch.softmax((torch.log(t * e_1 + (1 - t) * e_0) + gumbel_e) / tau_t(t), dim=-1)
-        #
-        #     mols = [make_molecule(x_t[i], e_t[i], max_nodes, True) for i in range(10)]
-        #     all_mols.extend(mols)
-        #
-        # # visualize RDKit grid
-        #
-        # grid = Chem.Draw.MolsToGridImage(all_mols, molsPerRow=10, subImgSize=(200, 200))
-        # grid.show()
-        # quit()
-
         t = t.unsqueeze(-1).unsqueeze(-1)
         x_t, v_x = conditional_velocity(distribution, x_1, t, tau_t, mu, sigma)
         t = t.unsqueeze(-1)
         e_t, v_e = conditional_velocity(distribution, e_1, t, tau_t, mu, sigma, edge=True)
         e_t[diag], v_e[diag] = 0, 0
-        #
-        # e_t_r, x_t_r = torch.argmax(e_t, dim=-1), torch.argmax(x_t, dim=-1)
-        # e_t_r, x_t_r = torch.nn.functional.one_hot(e_t_r, e_t.size(-1)), torch.nn.functional.one_hot(x_t_r, x_t.size(-1))
-        #
-        # x_t, y_t = add_feats(x_t, e_t_r, t, mask, device)
         y_t = torch.squeeze(t).unsqueeze(-1)
-
-        # x_t = torch.clone(x_t.detach()).to(device)
-        # e_t = torch.clone(e_t.detach()).to(device)
-        # v_x = torch.clone(v_x.detach()).to(device)
-        # v_e = torch.clone(v_e.detach()).to(device)
-        # y_t = y_t.to(device)
-        # mask = mask.to(device)
 
         # make prediction
         pred = model(x_t.float(), e_t.float(), y_t.float(), mask)
-       #  dis_# x = torch.cdist(pred.X, node_simplex, p=2)
-
-        # pred.X = torch.nn.functional.one_hot(x_pred, x_pred.size(-1))
-        # pred.E = torch.nn.functional.one_hot(e_gen, e_gen.size(-1))
 
         # get masks
         true_x = torch.reshape(x_1, (-1, x_1.size(-1))).to(device)
@@ -179,8 +114,6 @@ def train_graph_epoch(train, model, loader, optimizer, lr_scheduler, warmup_sche
         if loss_function == 'mse':
             target_x = torch.reshape(v_x, (-1, x_1.size(-1))).to(device)
             target_e = torch.reshape(v_e, (-1, e_1.size(-1))).to(device)
-            # target_x = torch.reshape(x_1, (-1, x_1.size(-1))).to(device)
-            # target_e = torch.reshape(e_1, (-1, e_1.size(-1))).to(device)
 
         elif loss_function == 'kld':
             target_x = torch.reshape(x_1, (-1, x_1.size(-1))).to(device)
